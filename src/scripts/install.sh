47f992dce4e3914b55ed1dfa274c6e41863d36c6
Carlos Tadeu Panato Junior <ctadeu@gmail.com>
2021-08-13 18:34:01 +0200

    [semver:major] fix previous PR due missing dev orb image (#9)
    Signed-off-by: Carlos Panato <ctadeu@gmail.com>

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -3,54 +3,61 @@ Install_Cosign() {
     expected_bootstrap_version_digest='e36a05ab402bfee5463ad4752d8dc2941204c7b01a9a9931f921e91d94ba2484'
     curl -L https://storage.googleapis.com/cosign-releases/v1.0.0/cosign-linux-amd64 -o cosign
     shaBootstrap=$(shasum -a 256 cosign | cut -d' ' -f1);
-    if [[ $shaBootstrap != ${expected_bootstrap_version_digest} ]]; then exit 1; fi
+    if [[ $shaBootstrap != "${expected_bootstrap_version_digest}" ]]; then exit 1; fi
     chmod +x cosign
 
     # If the bootstrap and specified `cosign` releases are the same, we're done.
-    if [[ ${{ inputs.cosign-release }} == ${bootstrap_version} ]]; then exit 0; fi
+    if [[ "${COSIGN_VERSION}" == "${bootstrap_version}" ]];
+    then
+        mkdir -p "$HOME"/.cosign && mv cosign "$HOME"/.cosign/
+        echo "export PATH=${HOME}/.cosign:$PATH" >> "$BASH_ENV"
+        source "$BASH_ENV"
+        cosign version
+        exit 0
+    fi
 
     semver='^v([0-9]+\.){0,2}(\*|[0-9]+)$'
-    if [[ ${{ inputs.cosign-release }} =~ $semver ]]; then
-        echo "INFO: Custom Cosign Version ${{ inputs.cosign-release }}"
+    if [[ ${COSIGN_VERSION} =~ $semver ]]; then
+        echo "INFO: Custom Cosign Version ${COSIGN_VERSION}"
     else
-        echo "ERROR: Unable to validate cosign version: '${{ inputs.cosign-release }}'"
+        echo "ERROR: Unable to validate cosign version: '${COSIGN_VERSION}'"
         exit 1
     fi
 
     # Download custom cosign
-    if [[ ${{ inputs.cosign-release }} == 'v0.6.0' ]]; then
-        curl -L https://storage.googleapis.com/cosign-releases/v0.6.0/cosign_linux_amd64 -o cosign_${{ inputs.cosign-release }}
+    if [[ "${COSIGN_VERSION}" == 'v0.6.0' ]]; then
+        curl -L https://storage.googleapis.com/cosign-releases/v0.6.0/cosign_linux_amd64 -o cosign_"${COSIGN_VERSION}"
     else
-        curl -L https://storage.googleapis.com/cosign-releases/${{ inputs.cosign-release }}/cosign-linux-amd64 -o cosign_${{ inputs.cosign-release }}
+        curl -L https://storage.googleapis.com/cosign-releases/"${COSIGN_VERSION}"/cosign-linux-amd64 -o cosign_"${COSIGN_VERSION}"
     fi
-    shaCustom=$(shasum -a 256 cosign_${{ inputs.cosign-release }} | cut -d' ' -f1);
+    shaCustom=$(shasum -a 256 cosign_"${COSIGN_VERSION}" | cut -d' ' -f1);
 
     # same hash means it is the same release
-    if [[ $shaCustom != $shaBootstrap ]];
+    if [[ $shaCustom != "$shaBootstrap" ]];
     then
-        if [[ ${{ inputs.cosign-release }} == 'v0.6.0' ]]; then
-        # v0.6.0's linux release has a dependency on `libpcsclite1`
-        sudo apt-get update -q
-        sudo apt-get install -yq libpcsclite1
-        curl -L https://github.com/sigstore/cosign/releases/download/v0.6.0/cosign_linux_amd64_0.6.0_linux_amd64.sig -o cosign-linux-amd64.sig
+        if [[ "${COSIGN_VERSION}" == 'v0.6.0' ]]; then
+            # v0.6.0's linux release has a dependency on `libpcsclite1`
+            sudo apt-get update -q
+            sudo apt-get install -yq libpcsclite1
+            curl -L https://github.com/sigstore/cosign/releases/download/v0.6.0/cosign_linux_amd64_0.6.0_linux_amd64.sig -o cosign-linux-amd64.sig
         else
-        curl -LO https://github.com/sigstore/cosign/releases/download/${{ inputs.cosign-release }}/cosign-linux-amd64.sig
+            curl -LO https://github.com/sigstore/cosign/releases/download/"${COSIGN_VERSION}"/cosign-linux-amd64.sig
         fi
-        if [[ ${{ inputs.cosign-release }} < 'v0.6.0' ]]; then
-        curl -L https://raw.githubusercontent.com/sigstore/cosign/${{ inputs.cosign-release }}/.github/workflows/cosign.pub -o release-cosign.pub
+        if [[ "${COSIGN_VERSION}" < 'v0.6.0' ]]; then
+            curl -L https://raw.githubusercontent.com/sigstore/cosign/"${COSIGN_VERSION}"/.github/workflows/cosign.pub -o release-cosign.pub
         else
-        curl -LO https://raw.githubusercontent.com/sigstore/cosign/${{ inputs.cosign-release }}/release/release-cosign.pub
+            curl -LO https://raw.githubusercontent.com/sigstore/cosign/"${COSIGN_VERSION}"/release/release-cosign.pub
         fi
-        ./cosign verify-blob -key release-cosign.pub -signature cosign-linux-amd64.sig cosign_${{ inputs.cosign-release }}
-        if [[ $? != 0 ]]; then exit 1; fi
+        ./cosign verify-blob -key release-cosign.pub -signature cosign-linux-amd64.sig cosign_"${COSIGN_VERSION}"
+        if [[ $? -ne 0 ]]; then exit 1; fi
         rm cosign
-        mv cosign_${{ inputs.cosign-release }} cosign
+        mv cosign_"${COSIGN_VERSION}" cosign
         chmod +x cosign
+        mkdir -p "$HOME"/.cosign && mv cosign "$HOME"/.cosign/
+        echo "export PATH=${HOME}/.cosign:$PATH" >> "$BASH_ENV"
+        source "$BASH_ENV"
+        cosign version
     fi
-    # Add to PATH
-    chmod +x cosign && mkdir -p "$HOME"/.cosign && mv cosign "$HOME"/.cosign/
-    echo "export PATH=${HOME}/.cosign:$PATH" >> "$BASH_ENV"
-    source "$BASH_ENV"
 }
 
 # Will not run if sourced for bats-core tests.
