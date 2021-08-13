b7b2d2527f1d1eba47d8643d66a0722d2810e1b3
Carlos Panato <ctadeu@gmail.com>
2021-08-13 17:18:21 +0200

    update to cosign 1.0.0
    Signed-off-by: Carlos Panato <ctadeu@gmail.com>

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -1,27 +1,51 @@
 Install_Cosign() {
-    curl -L https://storage.googleapis.com/cosign-releases/v0.2.0/cosign-linux-amd64 -o cosign
+    bootstrap_version='v1.0.0'
+    expected_bootstrap_version_digest='e36a05ab402bfee5463ad4752d8dc2941204c7b01a9a9931f921e91d94ba2484'
+    curl -L https://storage.googleapis.com/cosign-releases/v1.0.0/cosign-linux-amd64 -o cosign
     shaBootstrap=$(shasum -a 256 cosign | cut -d' ' -f1);
-    if [[ $shaBootstrap != "677f64870f209b1f41a02247a65697138973e3fb36a0836565cf173b9282aad6" ]]; then exit 1; fi
+    if [[ $shaBootstrap != ${expected_bootstrap_version_digest} ]]; then exit 1; fi
+    chmod +x cosign
+
+    # If the bootstrap and specified `cosign` releases are the same, we're done.
+    if [[ ${{ inputs.cosign-release }} == ${bootstrap_version} ]]; then exit 0; fi
 
     semver='^v([0-9]+\.){0,2}(\*|[0-9]+)$'
-    if [[ "${COSIGN_VERSION}" =~ $semver ]]; then
-        echo "INFO: Custom Cosign Version ${COSIGN_VERSION}"
+    if [[ ${{ inputs.cosign-release }} =~ $semver ]]; then
+        echo "INFO: Custom Cosign Version ${{ inputs.cosign-release }}"
     else
-        echo "ERROR: Unable to validate cosign version: '${COSIGN_VERSION}'"
+        echo "ERROR: Unable to validate cosign version: '${{ inputs.cosign-release }}'"
         exit 1
     fi
+
     # Download custom cosign
-    curl -L https://storage.googleapis.com/cosign-releases/"${COSIGN_VERSION}"/cosign-linux-amd64 -o cosign_"${COSIGN_VERSION}"
-    shaCustom=$(shasum -a 256 cosign_"${COSIGN_VERSION}" | cut -d' ' -f1);
-    # check if is the same hash means it is the same release
-    if [[ "$shaCustom" != "$shaBootstrap" ]];
+    if [[ ${{ inputs.cosign-release }} == 'v0.6.0' ]]; then
+        curl -L https://storage.googleapis.com/cosign-releases/v0.6.0/cosign_linux_amd64 -o cosign_${{ inputs.cosign-release }}
+    else
+        curl -L https://storage.googleapis.com/cosign-releases/${{ inputs.cosign-release }}/cosign-linux-amd64 -o cosign_${{ inputs.cosign-release }}
+    fi
+    shaCustom=$(shasum -a 256 cosign_${{ inputs.cosign-release }} | cut -d' ' -f1);
+
+    # same hash means it is the same release
+    if [[ $shaCustom != $shaBootstrap ]];
     then
-        chmod +x cosign
-        curl -L https://github.com/sigstore/cosign/releases/download/"${COSIGN_VERSION}"/cosign-linux-amd64.sig -o cosign.sig
-        curl -LO https://raw.githubusercontent.com/sigstore/cosign/"${COSIGN_VERSION}"/.github/workflows/cosign.pub
-        if ./cosign verify-blob --key cosign.pub --signature cosign.sig cosign_"${COSIGN_VERSION}"; then exit 1; fi
+        if [[ ${{ inputs.cosign-release }} == 'v0.6.0' ]]; then
+        # v0.6.0's linux release has a dependency on `libpcsclite1`
+        sudo apt-get update -q
+        sudo apt-get install -yq libpcsclite1
+        curl -L https://github.com/sigstore/cosign/releases/download/v0.6.0/cosign_linux_amd64_0.6.0_linux_amd64.sig -o cosign-linux-amd64.sig
+        else
+        curl -LO https://github.com/sigstore/cosign/releases/download/${{ inputs.cosign-release }}/cosign-linux-amd64.sig
+        fi
+        if [[ ${{ inputs.cosign-release }} < 'v0.6.0' ]]; then
+        curl -L https://raw.githubusercontent.com/sigstore/cosign/${{ inputs.cosign-release }}/.github/workflows/cosign.pub -o release-cosign.pub
+        else
+        curl -LO https://raw.githubusercontent.com/sigstore/cosign/${{ inputs.cosign-release }}/release/release-cosign.pub
+        fi
+        ./cosign verify-blob -key release-cosign.pub -signature cosign-linux-amd64.sig cosign_${{ inputs.cosign-release }}
+        if [[ $? != 0 ]]; then exit 1; fi
         rm cosign
-        mv cosign_"${COSIGN_VERSION}" cosign
+        mv cosign_${{ inputs.cosign-release }} cosign
+        chmod +x cosign
     fi
     # Add to PATH
     chmod +x cosign && mkdir -p "$HOME"/.cosign && mv cosign "$HOME"/.cosign/
